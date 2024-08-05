5541d7560232e1d595675eceddc4ff356c93ffe9
Danil-alt <kilindany24@gmail.com>
2024-08-05 19:08:15 +0500

    Update module_5_1_2.py

--- module_5_1.py
+++ module_5_1.py
@@ -16,8 +16,7 @@ class House:
         return self.number_of_floors
 
     def __str__(self):
-        string = str(f'Название: {self.name}, кол-во этажей: {self.number_of_floors}')
-        return string
+        return str(f'Название: {self.name}, кол-во этажей: {self.number_of_floors}')
 
 bigband = House('Бигбэнд', 21)
 tower = House('Башня', 99)
